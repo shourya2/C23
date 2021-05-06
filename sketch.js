b948fd78bdabb0b8be23d53af593c7430bf70f37
shourya2 <81694010+shourya2@users.noreply.github.com>
2021-05-06 14:56:06 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var ground, ball;
+
+function setup(){
+    var canvas = createCanvas(400,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    var ground_options ={
+        isStatic: true
+    }
+
+    ground = Bodies.rectangle(200,390,200,20,ground_options);
+    World.add(world,ground);
+
+   box1 = new Box(200,300,50,50);
+   box2 = new Box(220,100,50,100);
+
+    console.log(ground);
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+    rectMode(CENTER);
+    rect(ground.position.x,ground.position.y,400,20);
+    console.log(box1.body.angle);
+    console.log(box2.body.angle);
+
+    box1.display();
+    box2.display();
+}
